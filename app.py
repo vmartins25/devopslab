a5cc90270defdd6c78e054e23fe4ce1af47e3045
vmartins25 <valencar85@gmail.com>
2021-08-27 02:24:22 +0000

    App Python v1.0

---
+++ app.py
@@ -0,0 +1,12 @@
+print("Hello World")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def pagina_inicial():
+    return "Hello World"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
