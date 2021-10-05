90ae24ba211e85aaccd58f7f43f76a1a00a3c754
vmartins25 <58428683+vmartins25@users.noreply.github.com>
2021-10-05 09:22:03 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,26 +1,24 @@
-# -*- coding: utf-8 -*-                                                                                                                                                                                  
-from app import app                                                                                                                                                                                      
-import unittest                                                                                                                                                                                          
-                                                                                                                                                                                                         
-class Test(unittest.TestCase):                                                                                                                                                                           
-                                                                                                                                                                                                         
-    def setUp(self):                                                                                                                                                                                     
-        # cria uma instância do unittest, precisa do nome "setUp"                                                                                                                                        
-        self.app = app.test_client()                                                                                                                                                                     
-                                                                                                                                                                                                         
-        # envia uma requisicao GET para a URL                                                                                                                                                            
-        self.result = self.app.get('/')                                                                                                                                                                  
-                                                                                                                                                                                                         
-    def test_requisicao(self):                                                                                                                                                                           
-        # compara o status da requisicao (precisa ser igual a 200)                                                                                                                                       
-        self.assertEqual(self.result.status_code, 200)                                                                                                                                                   
-                                                                                                                                                                                                         
-    def test_conteudo(self):                                                                                                                                                                             
-        # verifica o retorno do conteudo da pagina                                                                                                                                                       
-        self.assertEqual(self.result.data.decode('utf-8'), "MBA Impacta Full Stack --DEVOPS na Veia")                                                                                                                                
-                                                                                                                                                                                                         
+# -*- coding: utf-8 -*-
+from app import app
+import unittest
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        # cria uma instância do unittest, precisa do nome "setUp"
+        self.app = app.test_client()
+
+        # envia uma requisicao GET para a URL
+        self.result = self.app.get('/')
+
+    def test_requisicao(self):
+        # compara o status da requisicao (precisa ser igual a 200)
+        self.assertEqual(self.result.status_code, 200)
+
+    def test_conteudo(self):
+        # verifica o retorno do conteudo da pagina
+        self.assertEqual(self.result.data.decode('utf-8'), "DevOps é sucesso - Vanessa")
+
 if __name__ == "__main__":                                                                                                                                                                               
     print ('INICIANDO OS TESTES')                                                                                                                                                                        
     print('----------------------------------------------------------------------')                                                                                                                      
     unittest.main(verbosity=2)
-
